b6ae8be6ee59c8a1caf5e2460f363487c13e1b03
Paul Engel <paul.engel@holder.nl>
2011-08-15 21:58:54 +0200

    Completed the demo page

--- demo/assets/script.js
+++ demo/assets/script.js
@@ -1,42 +1,57 @@
-//
-// $(function() {
-// $('textarea').each(function() {
-//   $(this).bind("keypress input beforepaste", {currId: this.id}, resizeEventHandler);
-//   resizeRefresh(this.id); // Initial refresh
-// });
-//
-// $('input.teaser-button').bind("click", resizeRefreshAll); // Fire on a click on the teaser split/join button
-//
-// function resizeEventHandler(event) {
-//   resizeRefresh(event.data.currId);
-// }
-//
-// function resizeRefreshAll() { // Split/join teaser has been clicked, refresh textareas
-//   $('textarea').each(function() {
-//     resizeRefresh(this.id);
-//   });
-// }
-//
-// function resizeRefresh(currId) {
-//   resizeTextarea($('textarea#' + currId));
-// }
-//
-// function resizeTextarea(textarea) {
-//   var lines = textarea.val().split("\n");
-//   var count = lines.length;
-//   $.each(lines, function() {
-//     count += parseInt(this.length / 74);
-//   });
-//
-//   textarea.css("height", count * 17);
-// }
-// });
+Event.on = function(element, event_type, callback, remove) {
+  if (element.attachEvent ? (remove ? element.detachEvent("on" + event_type, element[event_type + callback]) : 1) : (remove ? element.removeEventListener(event_type, callback, 0) : element.addEventListener(event_type, callback, 0))) {
+    element["e" + event_type + callback] = callback;
+    element[event_type + callback] = function() {
+      element["e" + event_type + callback](window.event);
+    };
+    element.attachEvent("on" + event_type, element[event_type + callback]);
+  }
+};
 
-var _=_?_:{}
-_.ready=_.R=function(f){"\v"=="v"?setTimeout(f,0):_.E(_.d,'DOMContentLoaded',f)}
-_.on=_.E=function(e,t,f,r){if(e.attachEvent?(r?e.detachEvent('on'+t,e[t+f]):1):(r?e.removeEventListener(t,f,0):e.addEventListener(t,f,0))){e['e'+t+f]=f;e[t+f]=function(){e['e'+t+f](window.event)};e.attachEvent('on'+t,e[t+f])}}
-_.d=document
+document.ready = function(callback) {
+  "\v" == "v" ? setTimeout(callback, 0) : Event.on(document, "DOMContentLoaded", callback);
+};
 
-_.R(function(){
-  // ready!
-})
+HTMLTextAreaElement.prototype.autoResize = function() {
+  var self = this;
+
+  var resize = function() {
+    var lines = self.value.split("\n");
+    var count = lines.length;
+    for (var i = 0; i < lines.length; i++) {
+      count += parseInt(lines[i].length / 74);
+    }
+    self.style.height = count * 17 + "px";
+  }
+
+  Event.on(this, "keypress", resize);
+  Event.on(this, "input", resize);
+  Event.on(this, "beforepaste", resize);
+  resize();
+
+  return this;
+};
+
+var element = null;
+
+function defineElement() {
+  element = document.createElement("div");
+  element.className = "element";
+  element.innerHTML = "This is the oned.js demo HTML element!";
+  element._onAdds = [];
+  element._onAdd(eval("function() {" +
+    document.getElementById("on_add").value + ";" +
+    "this._onAdds = [];" +
+  "}"));
+};
+
+function addElement() {
+  document.getElementById("page_content").appendChild(element);
+};
+
+document.ready(function() {
+  var on_add = document.getElementById("on_add");
+  on_add.autoResize();
+  on_add.focus();
+  defineElement();
+});
