c83dfcefd4d925349b4d2edf7d5ae76d92aa9cba
Paul Engel <paul.engel@holder.nl>
2011-09-13 22:34:12 +0200

    Completed the demo page for real this time (IE6+, Safari, FF and Chrome)

--- demo/assets/script.js
+++ demo/assets/script.js
@@ -1,4 +1,4 @@
-Event.on = function(element, event_type, callback, remove) {
+onEvent = function(element, event_type, callback, remove) {
   if (element.attachEvent ? (remove ? element.detachEvent("on" + event_type, element[event_type + callback]) : 1) : (remove ? element.removeEventListener(event_type, callback, 0) : element.addEventListener(event_type, callback, 0))) {
     element["e" + event_type + callback] = callback;
     element[event_type + callback] = function() {
@@ -9,48 +9,53 @@ Event.on = function(element, event_type, callback, remove) {
 };
 
 document.ready = function(callback) {
-  "\v" == "v" ? setTimeout(callback, 0) : Event.on(document, "DOMContentLoaded", callback);
-};
-
-HTMLTextAreaElement.prototype.autoResize = function() {
-  var self = this;
-
-  var resize = function() {
-    var lines = self.value.split("\n");
-    var count = lines.length;
-    for (var i = 0; i < lines.length; i++) {
-      count += parseInt(lines[i].length / 74);
-    }
-    self.rows = count;
-  }
-
-  Event.on(this, "keypress", resize);
-  Event.on(this, "input", resize);
-  Event.on(this, "beforepaste", resize);
-  resize();
-
-  return this;
+  "\v" == "v" ? setTimeout(callback, 0) : onEvent(document, "DOMContentLoaded", callback);
 };
 
 var element = null;
 
 function defineElement() {
-  element = document.createElement("div");
-  element.className = "element";
-  element.innerHTML = "This is the oned.js demo HTML element!";
-  element._onAdds = [];
-  element._onAdd(eval("(function() {" +
-    document.getElementById("on_add").value + ";" +
-    "defineElement();" +
-  "})"));
+  if (Oned.extendjQuery) {
+    element = $("<div>");
+    element.addClass("element");
+    element.html("This is the oned.js demo HTML element!");
+  } else {
+    element = document.createElement("div");
+    element.className = "element";
+    element.innerHTML = "This is the oned.js demo HTML element!";
+  }
+  element.onAdd(new Function(document.getElementById("on_add").value + ";\ndefineElement();"), true);
 };
 
 function addElement() {
-  document.getElementById("page_content").appendChild(element);
+  if (Oned.extendjQuery) {
+    $("#page_content").append(element);
+  } else {
+    document.getElementById("page_content").appendChild(element);
+  }
 };
 
 document.ready(function() {
   var on_add = document.getElementById("on_add");
+  on_add.autoResize = function() {
+    var self = this;
+
+    var resize = function() {
+      var lines = self.value.split("\n");
+      var count = lines.length;
+      for (var i = 0; i < lines.length; i++) {
+        count += parseInt(lines[i].length / 74);
+      }
+      self.rows = count;
+    }
+
+    onEvent(this, "keyup", resize);
+    onEvent(this, "input", resize);
+    onEvent(this, "beforepaste", resize);
+    resize();
+
+    return this;
+  }
   on_add.autoResize();
   on_add.focus();
   defineElement();
